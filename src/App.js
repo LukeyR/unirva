7efaf38bbeff147fed65ece414d2ca78fc55880e
LukeyR <robertsluke3@gmail.com>
2021-02-26 15:06:44 +0000

    Introduction to material UI and login screen

--- src/App.js
+++ src/App.js
@@ -4,28 +4,43 @@ import Product from './Product';
 import Chat from './Chat';
 import Home from './Home';
 import Profile from './Profile';
-import {Route} from 'react-router-dom';
+import {Route, useLocation} from 'react-router-dom';
 import NavBar from './hfRegion/NavBar';
 import Footer from './hfRegion/Footer';
+import Menu from "./authentication/Menu";
+
+function HideHeader() {
+    const location = useLocation();
+
+     if (location.pathname !== "/menu") {
+         return (
+             <>
+                 <NavBar/>
+                 <>Yes, whatever is put here is displayed on every page.</>
+             </>
+         )
+     } else {
+         return <></>
+     }
+}
 
 /**
  * This is like a template which will appear on every page of our website. Here we can insert all the backend for switching
  * between tabs as well as a header (nice design) which we can include on every page.
  */
-
 function App() {
+    const location = useLocation();
 
   // I used react-router-dom for switching between the pages so far (note that it should be installed using npm install)
   return (
       <div>
     <div className="App">
-      <NavBar />
-      <>Yes, whatever is put here is displayed on every page.</>
       <Route exact path="/Profile" component={Profile} />
       <Route exact path="/Home" component={Home} />
       <Route exact path="/Product" component={Product} />
       <Route exact path="/Chat" component={Chat} />
-      <Footer />
+    </div>
+          <Route exact path = "/Menu" component={Menu} />
     </div>
   );
 }
