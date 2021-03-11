9f39df90165efcaccce24e6a32ca983d9f924bbe
Paul Petan <pap36@bath.ac.uk>
2021-03-11 09:42:01 +0000

    solved a bug

--- src/Product.js
+++ src/Product.js
@@ -113,6 +113,7 @@ function Upload(){
                     imgUrl:url,
                     seller:user.uid,
                     createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+                    interestedUsers: ""
                 }).then(() => {
                     console.log('product submitted. redirecting...');
                     window.location.href = "/";
