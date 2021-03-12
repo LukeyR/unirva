a1fdd00e9f11fd33905b43d147c10551478bbd06
Paul Petan <pap36@bath.ac.uk>
2021-03-12 14:19:52 +0000

    Merged goToseller branch and upload image

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Menu from "./authentication/Menu";
 import Login from "./authentication/Login";
 import Logout from "./authentication/Logout";
 import SignIn from './authentication/SignIn';
+import SellerProfile from "./SellerProfile";
 import {AuthProvider} from './Auth';
 import {auth} from "./firebase";
 import {useAuthState} from 'react-firebase-hooks/auth';
@@ -114,6 +115,7 @@ function App() {
                     <Route exact path="/ChatRoom" component={ChatRoom}/>
                     <Route exact path="/Logout" component={Logout}/>
                     <Route exact path="/signout" component={Logout}/>
+                    <Route exact path="/SellerProfile" component={SellerProfile} />
                     {location.pathname === "/" ? <Home/> : <></>}
                     {HideFooter()}
                 </div>
