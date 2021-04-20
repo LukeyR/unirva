2fd065b9cd2ecebd2f15e42dd8aa436997ff7f7a
Luke Roberts <robertsluke3@gmail.com>
2021-04-20 19:25:55 +0100

    visual bug

--- src/Home.js
+++ src/Home.js
@@ -21,9 +21,8 @@ import UnfoldMoreIcon from '@material-ui/icons/UnfoldMore';
 import HomeListingCard from "./listingCard";
 import Skeleton from '@material-ui/lab/Skeleton';
 import brandLogo from "./img/Brandlogo.svg";
-import {useLocation} from "react-router-dom";
+import {Redirect, useLocation} from "react-router-dom";
 import {Alert} from "@material-ui/lab";
-import {Redirect} from "react-router-dom";
 
 const useStyles = makeStyles((theme) => (
     {
@@ -94,11 +93,11 @@ var sellersMatched = []
 
 function Home() {
     const [user] = useAuthState(auth);
-    if (user != null){
+    if (user != null) {
         console.log(user);
         if (user.emailVerified) verified = true;
         firestore.collection("users").doc(user.uid).get().then(doc => {
-            if(doc.data()) universityTarget = doc.data().University;
+            if (doc.data()) universityTarget = doc.data().University;
         })
         //console.log(verified);
     }
@@ -106,7 +105,7 @@ function Home() {
     // Getting the listings from the database.
     firestore.collection("users").get().then(snapshot => {
         snapshot.forEach(doc => {
-            if(doc.data().University == universityTarget && sellersMatched.includes(doc.id) == false){
+            if (doc.data().University == universityTarget && sellersMatched.includes(doc.id) == false) {
                 sellersMatched.push(doc.id);
             }
         })
@@ -259,12 +258,12 @@ function Home() {
                 selectedPriceIndex === 0
                 ||
                 (
-                    prices[selectedPriceIndex-1] <= listingObj.price
+                    prices[selectedPriceIndex - 1] <= listingObj.price
                     &&
                     listingObj.price <= prices[selectedPriceIndex]
                 )
             )
-            && 
+            &&
             (
                 sellersMatched.includes(listingObj.seller)
             )
@@ -356,141 +355,141 @@ function Home() {
 
     return (
         <div>
-        {user?
-        <div style={{ marginBottom: "100px" }}>
-            <Snackbar open={open} autoHideDuration={6000} onClose={handleCloseSnackbar}>
-                <Alert onClose={handleCloseSnackbar} severity="success" variant="filled">
-                    Your product was successfully submitted!
-                </Alert>
-            </Snackbar>
+            {user ?
+                <div style={{marginBottom: "100px"}}>
+                    <Snackbar open={open} autoHideDuration={6000} onClose={handleCloseSnackbar}>
+                        <Alert onClose={handleCloseSnackbar} severity="success" variant="filled">
+                            Your product was successfully submitted!
+                        </Alert>
+                    </Snackbar>
 
-            <Box display="flex" alignItems="center" justifyContent="center"
-                 style={{marginBottom: "-50px", marginTop: "-50px"}}>
-                <img src={brandLogo} alt="brand logo" width={40} height={40} className={classes.media}/>
-                <Typography variant="h2" style={{display: "inline", marginLeft: "15px", fontSize: "40px"}}
-                            color="textPrimary">
-                    unirva
-                </Typography>
-            </Box>
-
-            <Box p={1} m={1} style={{ marginRight: "56px", marginLeft: "56px",}}>
-                <Grid container justify="center" spacing={4}>
-                    <Grid item xs={12} md={4}>
-                <Box display="flex" justifyContent="flex-start" alignItems="center">
-                    <Typography display="inline" style={{marginRight: "10px"}}>
-                        Filter:
-                    </Typography>
-                    <Button onClick={handleClickListItemCategory} variant="outlined" color="primary">
-                        <Typography>
-                            {categoryOptions[selectedCategoryIndex]}
+                    <Box display="flex" alignItems="center" justifyContent="center"
+                         style={{marginBottom: "-50px", marginTop: "-50px"}}>
+                        <img src={brandLogo} alt="brand logo" width={40} height={40} className={classes.media}/>
+                        <Typography variant="h2" style={{display: "inline", marginLeft: "15px", fontSize: "40px"}}
+                                    color="textPrimary">
+                            unirva
                         </Typography>
-                        <UnfoldMoreIcon className={classes.moreIcon}/>
-                    </Button>
-                </Box>
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                <Box display="flex" justifyContent="center" alignItems="center">
-                    <Typography display="inline" style={{marginRight: "10px"}}>
-                        Price Range:
-                    </Typography>
-                    <Button onClick={handleClickListItemPrice} variant="outlined" color="primary">
-                        <Typography>
-                            {priceOptions[selectedPriceIndex]}
-                        </Typography>
-                        <UnfoldMoreIcon className={classes.moreIcon}/>
-                    </Button>
-                </Box>
-                    </Grid>
-                    <Grid item xs={12} md={4}>
-                <Box display="flex" justifyContent="flex-end" alignItems="center">
-                    <Typography display="inline" style={{marginRight: "10px"}}>
-                        Sort By:
-                    </Typography>
-                    <Button onClick={handleClickListItem} variant="outlined" color="primary">
-                        <Typography>
-                            {options[selectedIndex]}
-                        </Typography>
-                        <UnfoldMoreIcon className={classes.moreIcon}/>
-                    </Button>
-                </Box>
-                    </Grid>
-                </Grid>
-                <Menu
-                    id="filter-menu"
-                    anchorEl={anchorEl}
-                    keepMounted
-                    open={Boolean(anchorEl)}
-                    onClose={handleClose}
-                >
-                    {options.map((option, index) => (
-                        <MenuItem
-                            key={option}
-                            selected={index === selectedIndex}
-                            onClick={(event) => handleMenuItemClick(event, index)}
+                    </Box>
+
+                    <Box p={1} m={1} style={{marginRight: "56px", marginLeft: "56px",}}>
+                        <Grid container justify="center" spacing={4}>
+                            <Grid item xs={12} md={4}>
+                                <Box  display="flex" justifyContent="center" alignItems="center">
+                                    <Typography display="inline" style={{marginRight: "10px"}}>
+                                        Filter:
+                                    </Typography>
+                                    <Button onClick={handleClickListItemCategory} variant="outlined" color="primary">
+                                        <Typography>
+                                            {categoryOptions[selectedCategoryIndex]}
+                                        </Typography>
+                                        <UnfoldMoreIcon className={classes.moreIcon}/>
+                                    </Button>
+                                </Box>
+                            </Grid>
+                            <Grid item xs={12} md={4}>
+                                <Box display="flex" justifyContent="center" alignItems="center">
+                                    <Typography display="inline" style={{marginRight: "10px"}}>
+                                        Price Range:
+                                    </Typography>
+                                    <Button onClick={handleClickListItemPrice} variant="outlined" color="primary">
+                                        <Typography>
+                                            {priceOptions[selectedPriceIndex]}
+                                        </Typography>
+                                        <UnfoldMoreIcon className={classes.moreIcon}/>
+                                    </Button>
+                                </Box>
+                            </Grid>
+                            <Grid item xs={12} md={4}>
+                                <Box  display="flex" justifyContent="center" alignItems="center">
+                                    <Typography display="inline" style={{marginRight: "10px"}}>
+                                        Sort By:
+                                    </Typography>
+                                    <Button onClick={handleClickListItem} variant="outlined" color="primary">
+                                        <Typography>
+                                            {options[selectedIndex]}
+                                        </Typography>
+                                        <UnfoldMoreIcon className={classes.moreIcon}/>
+                                    </Button>
+                                </Box>
+                            </Grid>
+                        </Grid>
+                        <Menu
+                            id="filter-menu"
+                            anchorEl={anchorEl}
+                            keepMounted
+                            open={Boolean(anchorEl)}
+                            onClose={handleClose}
                         >
-                            {option}
-                        </MenuItem>
-                    ))}
-                </Menu>
-                <Menu
-                    id="category-menu"
-                    anchorEl={anchorElCategory}
-                    keepMounted
-                    open={Boolean(anchorElCategory)}
-                    onClose={handleClose}
-                >
-                    {categoryOptions.map((option, index) => (
-                        <MenuItem
-                            key={option}
-                            selected={index === selectedCategoryIndex}
-                            onClick={(event) => handleMenuItemClickCategory(event, index)}
+                            {options.map((option, index) => (
+                                <MenuItem
+                                    key={option}
+                                    selected={index === selectedIndex}
+                                    onClick={(event) => handleMenuItemClick(event, index)}
+                                >
+                                    {option}
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                        <Menu
+                            id="category-menu"
+                            anchorEl={anchorElCategory}
+                            keepMounted
+                            open={Boolean(anchorElCategory)}
+                            onClose={handleClose}
                         >
-                            {option}
-                        </MenuItem>
-                    ))}
-                </Menu>
-                <Menu
-                    id="price-menu"
-                    anchorEl={anchorElPrice}
-                    keepMounted
-                    open={Boolean(anchorElPrice)}
-                    onClose={handleClose}
-                >
-                    {priceOptions.map((option, index) => (
-                        <MenuItem
-                            key={option}
-                            selected={index === selectedPriceIndex}
-                            onClick={(event) => handleMenuItemClickPrice(event, index)}
+                            {categoryOptions.map((option, index) => (
+                                <MenuItem
+                                    key={option}
+                                    selected={index === selectedCategoryIndex}
+                                    onClick={(event) => handleMenuItemClickCategory(event, index)}
+                                >
+                                    {option}
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                        <Menu
+                            id="price-menu"
+                            anchorEl={anchorElPrice}
+                            keepMounted
+                            open={Boolean(anchorElPrice)}
+                            onClose={handleClose}
                         >
-                            {option}
-                        </MenuItem>
-                    ))}
-                </Menu>
+                            {priceOptions.map((option, index) => (
+                                <MenuItem
+                                    key={option}
+                                    selected={index === selectedPriceIndex}
+                                    onClick={(event) => handleMenuItemClickPrice(event, index)}
+                                >
+                                    {option}
+                                </MenuItem>
+                            ))}
+                        </Menu>
 
-            </Box>
-
-
-            <Box p={1} m={1}>
-                <Grid container justify="center" spacing={4}>
-                    {!loading ?
-                        ((showNew || showOld) ?
-                                ((showNew ? listings : listings.reverse()).map((listingObj, index) =>
-                                        getListingCard(listingObj, docsID[showNew ? index : docsID.length - 1 - index])
-                                ))
-                                :
-                                ((showLow ? listingsPrice : listingsPrice.reverse()).map((listingObj, index) =>
-                                        getListingCard(listingObj, docsID[showLow ? index : docsID.length - 1 - index])
-                                ))
+                    </Box>
 
 
-                        ) : emptyCards
-                    }
-                </Grid>
-            </Box>
-        </div>
-        :
-        <Redirect to="/login"></Redirect>
-        }
+                    <Box p={1} m={1}>
+                        <Grid container justify="center" spacing={4}>
+                            {!loading ?
+                                ((showNew || showOld) ?
+                                        ((showNew ? listings : listings.reverse()).map((listingObj, index) =>
+                                            getListingCard(listingObj, docsID[showNew ? index : docsID.length - 1 - index])
+                                        ))
+                                        :
+                                        ((showLow ? listingsPrice : listingsPrice.reverse()).map((listingObj, index) =>
+                                            getListingCard(listingObj, docsID[showLow ? index : docsID.length - 1 - index])
+                                        ))
+
+
+                                ) : emptyCards
+                            }
+                        </Grid>
+                    </Box>
+                </div>
+                :
+                <Redirect to="/login"></Redirect>
+            }
         </div>
     )
 }
